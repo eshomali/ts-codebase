84791556311f16a3f79f16d1dbccd069b79b7cee
eshomali <eshomaly@gmail.com>
2020-12-09 19:13:37 -0500

    resume cards + accordian

--- react-ui/src/structure/components/Resume.tsx
+++ react-ui/src/structure/components/Resume.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Card, CardContent, CardActions, Button, Typography, makeStyles} from '@material-ui/core';
+import {Card, CardContent, Accordion, AccordionSummary, AccordionDetails, Theme, Typography, makeStyles} from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStylesCard = makeStyles({
   root: {
     minWidth: 275,
   },
@@ -14,35 +14,69 @@ const useStyles = makeStyles({
     fontSize: 14,
   },
   pos: {
-    marginBottom: 12,
+    marginBottom: 12, 
   },
 });
+
+const useStylesAccordian = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
 
 interface Props {
   
 }
 
 export const Resume: React.FC = () => {
-  const classes = useStyles();
+  const classesCard = useStylesCard();
+  const classesAcc = useStylesAccordian();
   //const bull = <span className={classes.bullet}>•</span>;
   return (
    
-    <div>
-      <Card className={classes.root}>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          Comerica Bank
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          &nbsp;Engineer III
-        </Typography>
-        <Typography variant="body2" component="p">
-          Full Stack JavaScript
-          CI/CD
-          AWS
-        </Typography>
-      </CardContent>
-    </Card>
+    <div className="displayResume">
+      <Card className={classesCard.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Comerica Bank
+          </Typography>
+          <Typography className={classesCard.pos} color="textSecondary">
+            &nbsp;Engineer III
+          </Typography>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={''}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classesAcc.heading}>Summary</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                . did well I suppose
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={'<ExpandMoreIcon />'}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classesAcc.heading}>Skills</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                . cats
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </CardContent>
+      </Card>
     </div>
   );
 };
